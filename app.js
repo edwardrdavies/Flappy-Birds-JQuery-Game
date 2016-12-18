604bc38e9f86f8fad3f15d5ad4c15a79a587e9f1
edwardrdavies <edward_r_davies@hotmail.co.uk>
2016-12-18 11:23:52 +0000

    Set up intial DOM objects as varibles

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 $(function () {
-  var container = $('#container')
-})
+  //Saving DOM objects to varibles
+    var container = $('#container');
+    var bird = $('#bird');
+    var pole = $('#pole');
+    var pole_1 = $('#pole_1');
+    var pole_2 = $('#pole_2');
+    var score = $('#score');
+    var speed = $('#speed');
+    var restart_Btn = $('#restart_Btn');
+
+    //Saving some initial setup
+    var container_width = parseInt(container.width());
+    var container_height = parseInt(container.height());
+    var pole_initial_position = parseInt(pole.css('right'));
+    var pole_initial_height = parseInt(pole.css('height'));
+    var bird_left = parseInt(bird.css('left'));
+    var bird_height = parseInt(bird.css('height'));
+    var speed = 10;
+});
